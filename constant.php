e06b3169b311663e7c9443cb9c90f6cde70a7ab5
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-07-22 18:29:44 +0530

    removeed contact me

--- constant.php
+++
@@ -1,5 +0,0 @@
-<?php
-//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
-define('SITE_KEY',"6Lf6lLQZAAAAAAZLV3Q2CJubDzVQ3dqbHMZWYOgh");
-define('SECRET_KEY',"6Lf6lLQZAAAAAMQd-JGzCLLfrG6JncNUadUOv2yN")
-?>
