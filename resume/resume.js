4a930685aa7d59cc3969df286f2d3672a4e16958
Mudit Lal <46276282+Mudit-Lal@users.noreply.github.com>
2020-08-02 12:45:10 +0530

    Testing onclick change resume

---
+++ resume/resume.js
@@ -0,0 +1,83 @@
+function universe() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">The Answer</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function ccc() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">C & C++</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function python() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Python</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function photoshop() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Adobe Photoshop</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function ae() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Adobe After Effects</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function html() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">HTML 5</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function css() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">CSS 3</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function arduino() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Arduino</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function electronics() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Electronics</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function circuit() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Circuit Design</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function social() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Social Media Management</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
+
+function eventmanage() {
+  document.getElementById("skillname").innerHTML = "<b style=\"font-weight\: 700\;\">Event Management</b>";
+  document.getElementById("skillshortdesc").innerHTML = "was always";
+  document.getElementById("longdesc").innerHTML = "42";
+  document.getElementById("remove").innerHTML = "";
+}
